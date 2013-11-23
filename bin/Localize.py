d7ed5e34e813ef2af6981ddcb387e9fd2fd67e4f
Dung Pham <dpham05@berkeley.edu>
2013-11-23 04:26:30 -0800

    inner, outter circle, squares filled

--- bin/Localize.py
+++ bin/Localize.py
@@ -15,9 +15,9 @@ freq_room_mapping = ["", "kitchen", "bedroom", "hockey rink"]
 
 access_point_to_coord_mapping = {"access_point_1": (2, 8), "access_point_2": (-6, 0), "access_point_3": (61.3, 45.2) }
 
-room_to_constraint_mapping = {"kitchen": x >= 5 and x <= 8 and y >= 8 and y <= 10,
-                              "bedroom": x >=-8.7 and x <= -2 and y >= -5 and y <= 2.2,
-                              "hockey rink": x >=10000 and x <= 20000 and y >= -88888 and y <= -88293}
+room_to_constraint_mapping = {"kitchen": And(x >= 5, x <= 8, y >= 8, y <= 10),
+                              "bedroom": And(x >=-8.7, x <= -2, y >= -5, y <= 2.2),
+                              "hockey rink": And(x >=10000, x <= 20000, y >= -88888, y <= -88293)}
 
 
 ### Returns the radius appropriate for a given signal strength at a particular
@@ -77,17 +77,14 @@ if __name__ == '__main__':
     ultrasound_rooms = [room for (room, amp) in room_strength if amp > minimum_amplitude_cutoff and room != ""]
     print "Eliminating rooms below amplitude cutoff yields", ultrasound_rooms
     
-    u_clause = None 
+    u_clause = []
+    #neg_u_clause = []
     for room in ultrasound_rooms:
-        u_clause = u_clause or room_to_constraint_mapping[room]
-        # u_clause += "(" + room_to_constraint_mapping[room] + ")"
-        #if room != ultrasound_rooms[-1]:
-            #u_clause += " OR "
-    #u_clause += ")"
-    s.add(u_clause)
-    #s.check()
         
-    #print "ultrasound clause:" , u_clause
+        u_clause.append(room_to_constraint_mapping[room])
+        #neg_u_clause.append(Not(room_to_constraint_mapping[room]))
+    u_clause = Or(u_clause)   
+    #s.add(Or(u_clause))
 
     print "Identifying wifi access points"
     
@@ -110,29 +107,30 @@ if __name__ == '__main__':
     wifi_data = [ ("access_point_1", 25), ("access_point_2", 50) ]
     
     #wifi_clauses = ""
-    
+    u_clause = [u_clause]
     for (ap,ss)  in wifi_data:
         radius = radius_from_access_strength(ap,ss)
         #(x0, y0) = access_point_to_coord_mapping[ap]
         #print x0
-        s.add((x - access_point_to_coord_mapping[ap][0])**2 + (y - access_point_to_coord_mapping[ap][1])**2 <= radius_from_access_strength(ap,ss)**2 )
+        u_clause.append((x - access_point_to_coord_mapping[ap][0])**2 + (y - access_point_to_coord_mapping[ap][1])**2 <= radius_from_access_strength(ap,ss)**2)
+        #s.add((x - access_point_to_coord_mapping[ap][0])**2 + (y - access_point_to_coord_mapping[ap][1])**2 <= radius_from_access_strength(ap,ss)**2 )
         #if ((ap,ss) != wifi_data[-1]):
             #wifi_clauses += " AND "
-        
+    u_clause = And(u_clause)
+    #print u_clause
     #print "WiFi clauses:" , wifi_clauses
     
     print "Building SMT formula"
     
 
     #smt_formula = u_clause + wifi_clauses
-
     #print u_clause
-    
     #print "Sending to SMT solver:", smt_formula
     
     #TODO actually call the SMT solver
     print "Calling SMT solver ..."
-    
+
+    s.add(u_clause)
     print s.check()
     
     #TODO replace hardcoded results with SMT call
@@ -141,15 +139,109 @@ if __name__ == '__main__':
 
     # ------------------------------------------------------------------
     # Determine a circle which captures all possible value of location
-    r = 0
+    r = 1
+    #print s
     new_x = s.model()[x]
     new_y = s.model()[y]
+    
+    s.push()
+    s.add((x - new_x)**2 + (y - new_y)**2 >= r**2 )
+    
     while (s.check() == sat):
-        r = r + 1
+        print "SMT solver returns" , s.model(), "r =",r
+        r = r * 2
+        s.pop()
+        s.push()
         s.add((x - new_x)**2 + (y - new_y)**2 >= r**2 )
-        if s.check() == sat:
-            print "SMT solver returns" , s.model(), "r =",r
+    
+    s.pop()
+    range_list = [r/2, r]
+    print range_list
+    while(range_list[1] - range_list[0] > 0.1):
+        s.push()
+        s.add((x - new_x)**2 + (y - new_y)**2 >= ((range_list[1] - range_list[0])/2 + range_list[0])**2 )
+        if (s.check() == sat):
+            print "SMT solver returns" , s.model(), "r =", range_list
+            range_list[0] = (float(range_list[1]) - float(range_list[0]))/2 + range_list[0]
+        else:
+            range_list[1] = (float(range_list[1]) - float(range_list[0]))/2 + range_list[0]
+        s.pop()
+    print "Outter circle at point [", new_x, ", ", new_y, "] has radius: ", range_list[1]
+
     # -------------------------------------------------------------------
+    # ---------------------Determine inner circle------------------------
+
+    #print u_clause
+    s.reset()
+    s.add(Not(u_clause))
+
+    r = 1
+    #new_x = s.model()[x]
+    #new_y = s.model()[y]
+    
+    s.push()
+    s.add((x - new_x)**2 + (y - new_y)**2 <= r**2 )
+    
+    while (s.check() == unsat):
+        #print "SMT solver returns" , s.model(), "r =",r
+        r = r * 2
+        #print r
+        s.pop()
+        s.push()
+        s.add((x - new_x)**2 + (y - new_y)**2 <= r**2 )
+    
+    s.pop()
+    range_list = [float(r)/2, r]
+    while(range_list[1] - range_list[0] > 1):
+        s.push()
+        s.add((x - new_x)**2 + (y - new_y)**2 <= ((float(range_list[1]) - float(range_list[0]))/2 + range_list[0])**2 )
+        if (s.check() == unsat):
+            range_list[0] = (float(range_list[1]) - float(range_list[0]))/2 + range_list[0]
+        else:
+            range_list[1] = (float(range_list[1]) - float(range_list[0]))/2 + range_list[0]
+        s.pop()
+    print "Inner circle at point [", new_x, ", ", new_y, "] has radius: ", range_list[0]
+
+    # --------------------------------------------------------------------------
+    # ---------------------Fill up the whole region with square------------------------
+    # --------------------------------with maximum side-------------------------------
+
+    s.reset()
+    clause = (x**2 + y**2) <= 4
+    s.add(clause)
+    square_list = []
+    while(s.check() == sat):
+        print "a"
+        new_x = s.model()[x]
+        new_y = s.model()[y]
+        print s.model()
+        distance = 0.25
+        s.reset()
+        s.add(Not(clause))
+        print s
+        s.push()
+        s.add(x <= (new_x + distance), x >= (new_x - distance), y <= (new_y + distance), y >= (new_y - distance))
+        while(s.check() == unsat):
+            s.pop()
+            distance = distance * 2
+            s.push()
+            s.add(x <= (new_x + distance), x >= (new_x - distance), y <= (new_y + distance), y >= (new_y - distance))
+        s.pop()
+        range_list = [float(distance)/2, distance]
+        print range_list
+        while(range_list[1] - range_list[0] > 0.1):
+            s.push()
+            s.add(x <= (new_x + (float(range_list[1]) - float(range_list[0]))/2 + range_list[0]), x >= (new_x - (float(range_list[1]) - float(range_list[0]))/2 + range_list[0]), y <= (new_y + (float(range_list[1]) - float(range_list[0]))/2 + range_list[0]), y >= (new_y - (float(range_list[1]) - float(range_list[0]))/2 + range_list[0]))
+            if (s.check() == unsat):
+                range_list[0] = (float(range_list[1]) - float(range_list[0]))/2 + range_list[0]
+            else:
+                range_list[1] = (float(range_list[1]) - float(range_list[0]))/2 + range_list[0]
+            s.pop()
+        square_list.extend([x <= (new_x + range_list[1]), x >= (new_x - range_list[1]), y <= (new_y + range_list[1]), y >= (new_y - range_list[1])])                
+        s.reset()
+        s.add(clause)
+        s.add(Not(Or(square_list)))
+    print square_list
     
     # we could start by looking up this first satisfiable point,
     # or we can resample. # We have a bunch of options here, but for this,
